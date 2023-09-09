be501bc4220d41f204c73f4bc37904ef517a098c
Hong Phuc Le <72754942+lhplhp10102004@users.noreply.github.com>
2023-09-09 21:50:57 +0700

    Update program.py
    Using thread

--- HongPhuc/program.py
+++ HongPhuc/program.py
@@ -2,10 +2,17 @@ from model import HandGestureDetectionModel
 from data_processing import get_landmarks_from_images, draw_answer_to_image
 import cv2
 import default
+import threading
 
 class Program:
     def __init__(self, model):
         self.model = model
+    
+    def predict(self, frame):
+        landmarks = get_landmarks_from_images(frame)
+        if landmarks != None:
+            prediction = self.model.predict([landmarks])
+            print(default.gesture_name[prediction])
 
     def run(self):
         cap = cv2.VideoCapture(0)   # Initialize the webcam
@@ -13,21 +20,12 @@ class Program:
         while be_able_to_capture:
             be_able_to_capture, frame = cap.read()   # Read each frame from the webcam
             frame = cv2.flip(frame, 1)               # Flip the frame vertically
-
-            # Put prediction to the frame
-            landmarks = get_landmarks_from_images(frame)
-            if landmarks != None:
-                prediction = self.model.predict([landmarks])
-                draw_answer_to_image(frame, prediction)
-            else:
-                cv2.putText(frame, "No hand detected", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-            # Show the frame
             cv2.imshow("Frame", frame)
-
+            my_thread = threading.Thread(target=self.predict, args=(frame,))
+            my_thread.start()
+            # Show the frame
             # Press 'Esc' to exit
             if cv2.waitKey(1) == 27:
                 break
         cap.release()
         cv2.destroyAllWindows()
-        
-
